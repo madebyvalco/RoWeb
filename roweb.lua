3424905e40ff70eeab24fd5c5965c0da27646d93
Valco <80554644+RiseValco@users.noreply.github.com>
2022-12-22 06:46:24 -0500

    Update roweb.lua

--- roweb.lua
+++ roweb.lua
@@ -1,12 +1,13 @@
-local RoWeb = {}
+local RoWeb = {Version = "v0.0.2"}
 RoWeb.__index = RoWeb
 request = http_request or request or HttpPost or syn.request
 
 
-function RoWeb:new(url, options) 
-    local req = request({Url = url, Options = options})
+function RoWeb:new(url, options)
+    options.Url = url
+    local req = request(options)
     
-    local _page = {data = req, url = url, options = options}
+    local _page = {data = req, url = options.Url, options = options}
     setmetatable(_page, self)
     return _page
 end
@@ -18,7 +19,7 @@ end
 
 function RoWeb:getBody(options)
     if (options) and  (options.JSON) and options.JSON == true then 
-        local jsonData  = nil
+        local jsonData = nil
         local s, err = pcall(function() 
         local jsonTry = game:GetService("HttpService"):JSONDecode(self.data.Body)  
         jsonData = jsonTry
