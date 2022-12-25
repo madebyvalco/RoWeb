fe15cf303b5d77a83649561b25e0c409fb42c04e
Valco <80554644+RiseValco@users.noreply.github.com>
2022-12-25 04:22:29 -0500

    Update roweb.lua

--- roweb.lua
+++ roweb.lua
@@ -1,7 +1,13 @@
-local RoWeb = {Version = "v0.3.4"}
+local RoWeb = {Version = "v1.3.4"}
 RoWeb.__index = RoWeb
-request = http_request or request or HttpPost or syn.request
+_G.ID = math.random(1000, 2000)
 
+local reqName = syn and "syn.request" or
+http_request and "http_request" or
+request and "request" or
+HttpPost and "HttpPost"
+
+request = http_request or request or HttpPost or syn.request
 
 function RoWeb:new(url, options)
     local options = options or {}
@@ -41,6 +47,36 @@ end
 
 function RoWeb:getCookies()
     return self.data.Cookies
+end
+
+function RoWeb:spy(callback)
+    local instances = {
+        HttpGet = RoWeb.Version,
+        HttpGetAsync = RoWeb.Version,
+        HttpPost = RoWeb.Version,
+        HttpPostAsync = RoWeb.Version,
+        GetObjects = RoWeb.Version
+    }
+    local __nc
+    local QID = _G.ID
+     __nc = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+         if (_G.ID ~= QID) then return __nc(self, ...) end
+          local method = getnamecallmethod()
+          if (instances[method]) then
+              callback({logged = {method = method, data = "game:"..method..'("'.. ... .. '")'}})
+          end
+        return __nc(self, ...)
+     end))
+    local __rq
+    __rq = hookfunction(request, newcclosure(function(req)
+        if (_G.ID ~= QID) then return __rq(req) end
+        coroutine.wrap(function()
+            local s, res = pcall(__rq, req)
+            if (not s) then error("error played during getting response of hooking "..req.Url) end
+            callback({logged = {method = reqName, request = req, response = res}})
+        end)()
+        __rq(req)
+    end))
 end
 
 function RoWeb:getFingerprint() 
